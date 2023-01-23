91ef39a67794ea3a8b08c8beedfe86292b4b4e67
sharmaprasid <prasidpoudelsharma@gmail.com>
2023-01-23 18:18:22 +0545

    first commit

---
+++ server/index.js
@@ -0,0 +1,52 @@
+
+const express = require("express");
+const bodyParser= require('body-Parser');
+const cors= require('cors');
+const dotenv= require('dotenv');
+const multer= require('multer');
+const helmet= require('helmet');
+const morgan= require('morgan');
+
+const mongoose= require('mongoose');
+const path= require('path');
+
+
+
+/*config*/
+
+dotenv.config(); //use to envoke dotenv files
+const app= express()//envoke express and save instance as app
+app.use(express.json());
+app.use(helmet());//for ensuring safety request
+app.use(helmet.crossOriginResourcePolicy({policy:'cross-origin'}));
+app.use(morgan("common"))//used to handle log in
+app.use(bodyParser.json({limit:"30mb", extended:true}));
+app.use(bodyParser.urlencoded({limit:"30mb",extended:true}));
+app.use(cors())
+app.use("/assets", express.static(path.join(__dirname,'public/assets')));
+/*file Storage*/
+const storage = multer.diskStorage({
+    destination: function (req, file, cb) {
+      cb(null, '/public/assets')
+    },
+    filename: function (req, file, cb) {
+      
+      cb(null, file.originalname )
+    }
+  })
+  
+  const upload = multer({ storage })
+  const PORT=process.env.PORT||3002;
+  mongoose
+  .connect(process.env.MONGO_URL, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+  })
+  .then(() => {
+    app.listen(PORT, () => console.log(`Server Port: ${PORT}`));
+
+    /* ADD DATA ONE TIME */
+    // User.insertMany(users);
+    // Post.insertMany(posts);
+  })
+  .catch((error) => console.log(`${error} did not connect`));
